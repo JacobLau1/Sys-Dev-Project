ed995bab7417669f6087d7c54cd83a0b6ba5af7c
JacobLau1 <93946921+JacobLau1@users.noreply.github.com>
2023-04-25 22:17:14 -0400

    now spirit menu and beer menu can navigate back to menu selection page

--- Views/beermenu.php
+++ Views/beermenu.php
@@ -2,8 +2,8 @@
 
 <html>
     <body>
-        <a style="float:right" href="http://localhost/modavie/index.php?resource=user&action=login">Logout</a>
-        <a style="float:left" href="http://localhost/modavie/index.php?resource=user&action=menuselection">Back to menu selection</a>  
+        <a style='float:right' href='http://localhost/Sys-Dev-Project/index.php?resource=user&action=logout'>Logout</a>
+        <a style='float:left' href='http://localhost/Sys-Dev-Project/index.php?resource=user&action=menuselection'>Back to menu selection</a>
         <br>
         <h2>Beer Page</h2>
         <a href="http://localhost/modavie/index.php?resource=user&action=beerdisplay">Display</a>
--- Views/spiritmenu.php
+++ Views/spiritmenu.php
@@ -3,7 +3,7 @@
 <html>
     <body>
         <a style="float:right" href="http://localhost/modavie/index.php?resource=user&action=login">Logout</a>
-        <a style="float:left" href="http://localhost/modavie/index.php?resource=user&action=menuselection">Back to menu selection</a>  
+        <a style='float:left' href='http://localhost/Sys-Dev-Project/index.php?resource=user&action=menuselection'>Back to menu selection</a>
         <br>
         <h2>Spirits Page</h2>
         <a href="http://localhost/modavie/index.php?resource=user&action=spiritsdisplay">Display</a>
